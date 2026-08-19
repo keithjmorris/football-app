033f125230401b172a72844836b31b515e878e6f
keithjmorris <keith.morris@outlook.com>
2026-08-19 08:52:14 +0100

    Add complete PL and Championship team ID mapping

--- test-highlightly.mjs
+++ test-highlightly.mjs
@@ -1,7 +1,10 @@
-const res = await fetch('https://soccer.highlightly.net/events/1330185268', {
+const res = await fetch('https://soccer.highlightly.net/matches?leagueId=34824&season=2026&limit=100', {
   headers: { 'x-rapidapi-key': '38fc70e4-38e9-42af-96cc-f1566cdf2c1e' }
 });
 const data = await res.json();
-// Look for substitutions and their times
-const subs = data.filter(e => e.type === 'Substitution');
-console.log('Substitutions:', JSON.stringify(subs, null, 2));
+const teams = {};
+data.data?.forEach(m => {
+  teams[m.homeTeam.name] = m.homeTeam.id;
+  teams[m.awayTeam.name] = m.awayTeam.id;
+});
+console.log(JSON.stringify(teams, null, 2));
